fff961848df9b1c0ac6317b0c7053d4f3aca4c0c
cosmin <isaccosmin98@gmail.com>
2019-10-31 18:00:10 +0200

    Commit initial

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("Primul numar: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Al doilea numar: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} + {1} = {2}", a,b,a+b);
+            Console.WriteLine("{0} * {1} = {2}", a, b, a * b);
+        }
+    }
+}
